4eb3d7c37f44599bc0df921259464826085db713
baizhantang <baizhantang@pinduoduo.com>
2018-07-22 11:45:15 +0800

    '更新server'

--- server/server.js
+++ server/server.js
@@ -1,19 +1,16 @@
-const express = require('express');
-const mongoose = require('mongoose');
+const express = require('express')
+const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
 
-const DB_URL = 'mongodb://127.0.0.1/xyChat';
-mongoose.connect(DB_URL);
-mongoose.connection.on('connected',function(){
-    console.log('mongoDB connect success');
-});
-const app = express();
+const userRouter = require('./user')
 
-app.get('/', function (req, res) {
-    res.send('<h1>hello xyChat</h1>')
+const app = express()
+app.use(cookieParser())
+app.use(bodyParser.json())
+app.use('/user',userRouter)
+app.listen(9093,function(){
+	console.log('Node app start at port 9093')
 })
-app.get('/data', function (req, res) {
-    res.send('123456789456')
-})
-app.listen(9093, function () {
-    console.log('Node app start at port 9093')
-})
+
+
+
