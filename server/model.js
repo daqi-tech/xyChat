3ba37a67dec770e4c97385dfd6d406a5239d121a
baizhantang <baizhantang@pinduoduo.com>
2018-12-16 20:57:42 +0800

    update

--- server/model.js
+++ server/model.js
@@ -1,6 +1,6 @@
-const mongoose = require('mongoose')
-// 链接mongo 并且使用imooc这个集合
-const DB_URL = 'mongodb://192.168.125.128:27017/xychat'
+const mongoose = require('mongoose');
+// 链接mongo 并且使用imooc这个集合;
+const DB_URL = 'mongodb://127.0.0.1:27017/xychat';
 mongoose.connect(DB_URL)
 
 const models = {
@@ -10,11 +10,10 @@ const models = {
 		'type':{'type':String, 'require':true},
 		//头像
 		'avatar':{'type':String},
-		// 个人简介或者职位简介
+		// 简介
 		'desc':{'type':String},
-		// 职位名
 		'title':{'type':String},
-		// 如果你是boss 还有两个字段
+		// 如果你是XX 还有两个字段
 		'company':{'type':String},
 		'money':{'type':String}
 	},
